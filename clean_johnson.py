f0d50b9f75c805ce569ba1638e36dc44e7a4921f
Nat Alison <tesseralis@gmail.com>
2015-05-17 00:11:53 -0700

    Rename the data files.

--- clean_johnson.py
+++ clean_johnson.py
@@ -1,15 +1,16 @@
 import json
 
-with open('johnson.json') as f:
+with open('data/johnson-metadata.json') as f:
   johnson_data = json.load(f)['polyhedra']
 
 johnson_solids = []
 for i in xrange(1, 93):
-  with open('johnson-json/j' + str(i) + '.json', 'r') as f:
+  with open('data/johnson-json/j' + str(i) + '.json', 'rw') as f:
     text = f.read()
     text = text.replace("'", '"') # whoops, I forgot to use the right quotes
     solid = json.loads(text)
     solid['name'] = johnson_data[i-1]['name']
+    #print json.dumps(solid)
     johnson_solids.append(solid)
 
 print json.dumps(johnson_solids, indent=2)
