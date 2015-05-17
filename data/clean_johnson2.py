da740466b0c92b5e320e526a2be2fbf5405b8664
Nat Alison <tesseralis@gmail.com>
2015-05-16 23:22:22 -0700

    Add the Johnson solid name

--- data/clean_johnson2.py
+++ data/clean_johnson2.py
@@ -1,10 +1,15 @@
 import json
 
-johnson = []
+with open('johnson.json') as f:
+  johnson_data = json.load(f)['polyhedra']
+
+johnson_solids = []
 for i in xrange(1, 93):
   with open('johnson-json/j' + str(i) + '.json', 'r') as f:
     text = f.read()
     text = text.replace("'", '"') # whoops, I forgot to use the right quotes
-    johnson.append(json.loads(text))
+    solid = json.loads(text)
+    solid['name'] = johnson_data[i-1]['name']
+    johnson_solids.append(solid)
 
-print json.dumps(johnson, indent=2)
+print json.dumps(johnson_solids, indent=2)
