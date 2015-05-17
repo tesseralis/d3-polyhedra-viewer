ed9d0c545067d08b02be83e3aff921ccf1cec167
Nat Alison <tesseralis@gmail.com>
2015-05-17 00:15:07 -0700

    Make the Johnson solid data sane.

--- clean_johnson.py
+++ clean_johnson.py
@@ -3,14 +3,15 @@ import json
 with open('data/johnson-metadata.json') as f:
   johnson_data = json.load(f)['polyhedra']
 
-johnson_solids = []
+#johnson_solids = []
 for i in xrange(1, 93):
-  with open('data/johnson-json/j' + str(i) + '.json', 'rw') as f:
+  with open('data/johnson-json/j' + str(i) + '.json', 'r') as f:
     text = f.read()
     text = text.replace("'", '"') # whoops, I forgot to use the right quotes
     solid = json.loads(text)
     solid['name'] = johnson_data[i-1]['name']
-    #print json.dumps(solid)
-    johnson_solids.append(solid)
+  with open('data/johnson-json/j' + str(i) + '.json', 'w') as f:
+    json.dump(solid, f)
+    #johnson_solids.append(solid)
 
-print json.dumps(johnson_solids, indent=2)
+#print json.dumps(johnson_solids, indent=2)
