29048864b4e1f8ab3c2b31dd8925d80f2122f040
Nat Alison <tesseralis@gmail.com>
2015-05-19 22:36:20 -0700

    Add JSON data for other polyhedra.

--- data/merge_johnson.py
+++ data/merge_johnson.py
@@ -6,7 +6,7 @@ import json
 
 johnson_solids = []
 for i in xrange(1, 93):
-  with open('johnson-json/j' + str(i) + '.json', 'r') as f:
+  with open('polyhedra/johnson/j' + str(i) + '.json', 'r') as f:
     johnson_solids.append(json.load(f))
     
 
